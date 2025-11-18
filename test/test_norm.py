3d15b93681fef6a9fed5f2c9be43c0d605aa9a47
c8ef <c8ef@outlook.com>
2025-11-19 00:34:52 +0800

    fix test

--- test/test_norm.py
+++ test/test_norm.py
@@ -2,15 +2,21 @@ import torch
 
 from attotron.nn.norm import FlashRMSNorm, LlamaRMSNorm
 
-batch_size = 4
-seq_len = 128
-hidden_size = 768
-eps = 1e-5
 
-flash_norm = FlashRMSNorm(hidden_size, eps)
-llama_norm = LlamaRMSNorm(hidden_size, eps)
+def test_norm():
+    batch_size = 4
+    seq_len = 128
+    hidden_size = 768
+    eps = 1e-5
 
-x = torch.randn(batch_size, seq_len, hidden_size, dtype=torch.float32)
-out_flash = flash_norm(x)
-out_llama = llama_norm(x)
-assert torch.allclose(out_flash, out_llama)
+    flash_norm = FlashRMSNorm(hidden_size, eps).cuda()
+    llama_norm = LlamaRMSNorm(hidden_size, eps).cuda()
+
+    x = torch.randn(batch_size, seq_len, hidden_size, dtype=torch.float32).cuda()
+    out_flash = flash_norm(x)
+    out_llama = llama_norm(x)
+    assert torch.allclose(out_flash, out_llama)
+
+
+if __name__ == "__main__":
+    test_norm()
